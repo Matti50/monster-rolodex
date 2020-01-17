bd8ef426566bb20467b47d34d31df6b45fcf0e4e
mati <matiascicchittimg@gmail.com>
2020-01-17 14:07:40 -0300

    Updates

---
+++ precache-manifest.d30fe5c74624e8a00e35ca73b076459b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdae11a5167cd0b089c3d2447f793d38",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1851fd03d8007ffcc29c",
+    "url": "/monster-rolodex/static/css/main.a582f657.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monster-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1851fd03d8007ffcc29c",
+    "url": "/monster-rolodex/static/js/main.229cab87.chunk.js"
+  },
+  {
+    "revision": "8ac8ffe081e22d695c33",
+    "url": "/monster-rolodex/static/js/runtime-main.7098c1e8.js"
+  }
+]);
